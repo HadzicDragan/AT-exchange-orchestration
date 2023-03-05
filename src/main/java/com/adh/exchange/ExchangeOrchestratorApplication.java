3b45f0ae9ac5840d16f8c01f6ab28681b0ccbffb
Dragan Hadzic <hadzic.dragan9@gmail.com>
2023-03-05 02:18:00 +0100

    - Maven project structure

---
+++ src/main/java/com/adh/exchange/ExchangeOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.adh.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adh/exchange/ExchangeOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adh.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
